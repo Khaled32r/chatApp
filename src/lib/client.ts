f59f62e10015cf1c7205f4ebfe35b84cf9509c6f
Khaled Alahmad <115147841+Khaled32r@users.noreply.github.com>
2026-03-12 04:52:39 +0300

    Update client URL to remove protocol

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysiajs/eden"
 import type { App } from "../app/api/[[...slugs]]/route"
 
-export const client = treaty<App>("https://chat-app-ten-self.vercel.app").api
+export const client = treaty<App>("chat-app-ten-self.vercel.app").api
